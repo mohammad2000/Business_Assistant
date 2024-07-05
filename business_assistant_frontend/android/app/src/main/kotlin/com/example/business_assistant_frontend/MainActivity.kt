4d58637e09cadf07dd168961dd86525600cc5a87
mohammad <m.salamati2000@gmail.com>
2024-07-05 15:17:46 -0400

    first commit

---
+++ business_assistant_frontend/android/app/src/main/kotlin/com/example/business_assistant_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.business_assistant_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ business_assistant_frontend/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app)
+}
+
+class myApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+return new MaterialApp(
+  title: "test",
+  home: ,
+);
+  }
+
+
+}
+
+class homePage{
+  
+}
